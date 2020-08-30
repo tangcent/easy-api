ddd57a79fd3eaf3c0770d25146ac7061138894d5
tangcent <pentatangcent@gmail.com>
2020-08-30 18:48:26 +0800

    opti: use safeComputeIfAbsent instead of computeIfAbsent (#329)

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/DefaultMethodInferHelper.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/DefaultMethodInferHelper.kt
@@ -336,9 +336,11 @@ class DefaultMethodInferHelper : MethodInferHelper {
 
     fun tryInfer(infer: Infer): Any? {
         actionContext!!.checkStatus()
-        //find recursive call
-        methodStack.filter { it.callMethod() == infer.callMethod() }
-                .forEach { return it.possible() }
+        try {//find recursive call
+            methodStack.filter { it.callMethod() == infer.callMethod() }
+                    .forEach { return it.possible() }
+        } catch (ignore: Exception) {
+        }
         try {
             methodStack.push(infer)
             return infer.infer()
