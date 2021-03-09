9c48f0d5383aad1fa783a81389d6e60099cfedbd
tangcent <pentatangcent@gmail.com>
2021-03-09 21:53:44 +0800

    feat: parse `Header`&`Param` by `ExtensibleKit.fromJson`

--- common-api/src/main/kotlin/com/itangcent/common/constant/Attrs.kt
+++ common-api/src/main/kotlin/com/itangcent/common/constant/Attrs.kt
@@ -8,9 +8,7 @@ object Attrs {
 
     const val DEFAULT_VALUE_ATTR = "@default"
 
-    const val DEMO_ATTR = "@demo"
-
-    const val PREFIX = '@'
+    const val PREFIX = "@"
 
     val ALL = arrayOf(COMMENT_ATTR, REQUIRED_ATTR, DEFAULT_VALUE_ATTR)
 }
